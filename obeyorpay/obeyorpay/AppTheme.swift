3fb028f8470d89a7d830beed44bb6418f9907931
Mariia Turchina <mariia.turchina@gmail.com>
2022-09-30 14:22:07 +0200

    changed colors

--- obeyorpay/obeyorpay/AppTheme.swift
+++ obeyorpay/obeyorpay/AppTheme.swift
@@ -18,18 +18,16 @@ class AppTheme {
     var unselectedTextColor = Color(red: 181.0/255.0, green: 186.0/255.0, blue: 196.0/255.0)
     var lightTextColor = Color.white
     var taskColors = [
-        Color(red: 183/255, green: 220/255, blue: 225/255),
-        //Color(red: 214/255, green: 235/255, blue: 238/255),
-        Color(red: 238/255, green: 190/255, blue: 203/255),
-        Color(red: 244/255, green: 214/255, blue: 222/255),
         Color(red: 201/255, green: 173/255, blue: 255/255),
-        Color(red: 217/255, green: 198/255, blue: 255/255),
+        Color(red: 242/255, green: 145/255, blue: 171/255),
         Color(red: 253/255, green: 223/255, blue: 126/255),
-        Color(red: 255/255, green: 255/255, blue: 181/255),
-        Color(red: 190/255, green: 241/255, blue: 206/255),
-        Color(red: 217/255, green: 255/255, blue: 229/255),
+        Color(red: 149/255, green: 198/255, blue: 185/255),
+        Color(red: 238/255, green: 190/255, blue: 203/255),
+        Color(red: 172/255, green: 154/255, blue: 155/255),
+        Color(red: 183/255, green: 220/255, blue: 225/255),
         Color(red: 255/255, green: 184/255, blue: 109/255),
-        Color(red: 255/255, green: 216/255, blue: 173/255)
+        Color(red: 154/255, green: 234/255, blue: 179/255),
+        Color(red: 121/255, green: 180/255, blue: 242/255)
     ]
     var buttonColor = Color(red: 88/255, green: 218/255, blue: 127/255)
     var alertSize: (CGFloat, CGFloat) = (150, 160)
